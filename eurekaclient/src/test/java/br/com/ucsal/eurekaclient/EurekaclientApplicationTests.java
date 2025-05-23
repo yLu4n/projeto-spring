d242af809efaa9d94834f178623013f12d2ba3f6
Luan Victor <107703846+yLu4n@users.noreply.github.com>
2025-05-23 09:00:20 -0300

    Server e MsClient config

---
+++ eurekaclient/src/test/java/br/com/ucsal/eurekaclient/EurekaclientApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ucsal.eurekaclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaclientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
